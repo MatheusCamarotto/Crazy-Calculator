49f50b8a5f26598b6f61ec53a3d176b6a13334a8
Matheus Camarotto Manfio Domingues <ext.matheus.domingues@eldoradobrasil.com.br>
2026-07-28 09:15:37 -0400

    feat: finalizando configuração do server e teste da primeira rota

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -3,3 +3,9 @@
 from flask import Blueprint, jsonify, request
 
 calc_routes_bp = Blueprint("calc_routes", __name__) #isso nomeia as rotas das calculadoras e categoriza elas de fato como as rotas especificas da calculadora
+
+@calc_routes_bp.route("/calculator/1", methods=["POST"])
+def calculator_1():
+    print(request)
+    print(request.json)# pega o boddy da requisição
+    return jsonify({"Sucess": True}), 200
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,8 @@
 #Definições básicas para o servidor
 from flask import Flask
+from src.main.routes.calculators import calc_routes_bp
 
 app = Flask(__name__)
+
+#cadastrando a blueprint no app
+app.register_blueprint(calc_routes_bp)
