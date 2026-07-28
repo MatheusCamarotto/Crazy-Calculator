98c2fa4b1308027048ad919e794afc0a6963014d
Matheus Camarotto Manfio Domingues <ext.matheus.domingues@eldoradobrasil.com.br>
2026-07-28 10:04:17 -0400

    feat: maneira complicada de se finalizar a tarefa, porém feito todos os processos da calculadora

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -5,12 +5,16 @@ from typing import Dict
 class Calculator1:
     """Função da calculadora 1
     1. Um número é divido em 3 partes iguais.
+    __first_process()
     2.1. A primeira parte é dividida por 4 e seu resultado somado a 7.
     2.2. Após isso, o resultado é elevado ao quadrado e multiplicado
     por um valor de 0.257.
+    __second_process()
     3.1. A segunda parte é elevada a potência de 2.121, dividida 
     por 5 e somado a 1.
     3.2. A terceira parte se mantem no mesmo valor.
+
+    4. Por fim é somado os 3 valores e entregue o resultado.
     """
 
     def calculate(self, request:FlaskRequest) -> Dict:
@@ -18,8 +22,13 @@ class Calculator1:
         input_data = self.__validate_body(body)
         splited_number = input_data / 3
 
-        first_process_result = self.__first_process(splited_number)
-        
+        first_process_result = self.__first_process(first_number=splited_number)
+
+        second_process_result = self.__second_process(second_number=splited_number)
+
+        third_process_result = self.__third_process(third_number=splited_number)
+
+        final_process_result = self.__final_process(firts_result=first_process_result, second_result=second_process_result, third_result=third_process_result)
 
     def __validate_body(self, body: Dict) -> float:
         if "number" not in body:
@@ -32,3 +41,15 @@ class Calculator1:
         first_part = (first_number/4) + 7
         second_part = (first_part**2) * 0.257
         return second_part
+
+    def __second_process(self, second_number: float) -> float:
+        first_part = (second_number**2.121) 
+        second_part = (first_part / 5) + 1
+        return second_part
+
+    def __third_process(self, third_number: float) -> float:
+        return third_number
+
+    def __final_process(self, first_result: float, second_result: float, third_result: float) -> float:
+        first_part = first_result + second_result + third_result
+        return first_part
