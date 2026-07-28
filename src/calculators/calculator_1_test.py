a99b140294377ab1fa3d968202aed5d6f4a3b674
Matheus Camarotto Manfio Domingues <ext.matheus.domingues@eldoradobrasil.com.br>
2026-07-28 10:49:29 -0400

    test: introdução aos testes

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,16 @@
+def test_something():
+    print()
+    print("Rodando no teste 1")
+
+def test_something2():
+    print()
+    print("Rodando no teste 2")
+
+"""pytest
+É necessário ter o "test" no começo ou final dos arquivos, funções, métodos, tudo relacionado ao pytest
+precisar ter "test" para poder ser reconhecido pela lib.
+
+para poder ver os retornos de maneira mais detalhada
+pytest -s -v
+-v: verbose
+"""
