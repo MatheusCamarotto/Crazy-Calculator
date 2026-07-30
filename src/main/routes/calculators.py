b209a0e645afd5cebc11867403c8d6c292f13041
Matheus Camarotto Manfio Domingues <ext.matheus.domingues@eldoradobrasil.com.br>
2026-07-30 13:39:16 -0400

    feat: adicionando padrões de fabrica e criação de calculadoras de forma organizada

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,26 +1,21 @@
 #Rotas para as calculadoras
 
 from flask import Blueprint, jsonify, request
-
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator2_factory import calculator2_factory 
+from src.main.factories.calculator1_factory import calculator1_factory
 
 calc_routes_bp = Blueprint("calc_routes", __name__) #isso nomeia as rotas das calculadoras e categoriza elas de fato como as rotas especificas da calculadora
 
 @calc_routes_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calc = Calculator1()
+    calc = calculator1_factory()
     response = calc.calculate(request=request)
 
-    # print(request)
-    # print(request.json)# pega o boddy da requisição
     return jsonify(response), 200
 
 @calc_routes_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calc = Calculator2(numpy_handler)
+    calc = calculator2_factory()
     response = calc.calculate(request=request)
 
     return jsonify(response), 200
